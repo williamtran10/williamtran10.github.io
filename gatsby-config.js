6487f064ea76da482862a9c992004bee09c63701
William <williambrandontran@gmail.com>
2021-02-10 14:42:19 -0500

    Revert "Update names"
    This reverts commit 6f41dfbc82039f2310dceae5e83dca2ddeb79fd8.

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,8 +22,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `William Tran`,
-        short_name: `William Tran`,
+        name: `Gatsby Simplefolio`,
+        short_name: `Simplefolio`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#02aab0`,
