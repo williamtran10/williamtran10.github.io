6f41dfbc82039f2310dceae5e83dca2ddeb79fd8
William <williambrandontran@gmail.com>
2021-02-10 14:21:52 -0500

    Update names

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,8 +22,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Simplefolio`,
-        short_name: `Simplefolio`,
+        name: `William Tran`,
+        short_name: `William Tran`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#02aab0`,
